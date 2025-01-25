4b7eb54a8e7551401ca3bc618a1ece788da9942d
Neroil <edwin.haffner@heig-vd.ch>
2025-01-25 13:08:26 +0100

    removed test

--- src/utils/input_validation.rs
+++ src/utils/input_validation.rs
@@ -6,13 +6,6 @@ use zxcvbn::Score::Three;
 
 extern crate zxcvbn;
 
-// Added for testing purposes
-impl Username {
-    pub fn new(username: String) -> Username {
-        Username(username)
-    }
-}
-
 /// This function checks if the given password is valid
 /// Returns true if the password is strong enough, false otherwise
 fn password_validation(password: &str, username: &str) -> bool {
